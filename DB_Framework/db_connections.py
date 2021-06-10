d0f5e6c0feaea1e35ecc86a34df69126dd7e25b3
hamid-kiavarz <hkiavarz@erisinfo.com>
2021-06-10 17:33:11 -0400

    deploy GIS framework inor Prod

---
+++ DB_Framework/db_connections.py
@@ -0,0 +1,33 @@
+
+import arcpy,os
+import ConfigParser
+
+server_environment = 'Prod'
+server_config_file = r'\\cabcvan1gis006\GISData\ERISServerConfig.ini'
+connection_string = r'eris_gis/gis295@cabcvan1ora006.glaciermedia.inc:1521/GMPRODC'
+order_fc = "//cabcvan1gis006/GISData/GIS_Prod.sde/ERIS.ERIS_ORDER_GEOMETRY"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
